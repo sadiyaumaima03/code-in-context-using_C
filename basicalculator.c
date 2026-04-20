048b97a22bc4450dddcac629a34d574d817529cc
Umaima Sadiya <sadiyaumaima8@gmail.com>
2026-04-20 22:55:12 +0530

    Basic_Calculator

---
+++ basicalculator.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+    int main(){
+        int a,b;
+        char op;
+        printf("Enter two numbers:");
+        scanf("%d %d",&a , &b);
+        printf("Enter operator:");
+        scanf(" %c",&op);
+
+        switch(op){
+        case '+':
+            printf("Addition is %d", a+b);
+            break;
+        case '-':
+            printf("Difference is %d", a-b);
+            break;
+        case '*':
+            printf("Multiplication is %d", a*b);
+            break;
+        case '%':
+            printf("Remainder is %d", a%b);
+        case '/':
+            if(b!=0){
+                printf("Quotient is %d", a/b);
+            }else{
+                printf("ERROR: Division by zero");
+            }
+        default: 
+            printf("Invalid operator");
+        }
+    return 0;
+    }
