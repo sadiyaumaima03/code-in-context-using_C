06d2dc4081915c98d8f09a81b3cbb7cd82d9933f
Umaima Sadiya <sadiyaumaima8@gmail.com>
2026-05-05 22:15:30 +0530

    Guessing_Game

---
+++ guessingame.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+    int main(){
+        int secret, guess;
+
+        printf("------WELCOME TO THE GUESS GAME------\n");
+        printf("Enter your guess:");
+        
+        srand(time(0));
+        secret = rand() % 100 + 1;
+        
+        while(guess != secret){
+            scanf("%d",&guess);
+            if(guess < secret){
+                printf("Your guess is too low\n");
+            } else if(guess > secret){
+                printf("Your guess is too high\n");
+            }
+        }
+        printf("Guessed correctly : %d\n",secret);
+    }
